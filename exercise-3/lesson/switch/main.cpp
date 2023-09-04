ad96e322d868f6103f9ce2269b70820def7fe1b3
MariyaEM1 <72265069+MariyaEM1@users.noreply.github.com>
2023-09-04 22:27:00 +0300

    Create main.cpp

---
+++ exercise-3/lesson/switch/main.cpp
@@ -0,0 +1,53 @@
+// C++ program to demonstrate syntax of switch
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //Use the switch statement to select one of many code blocks to be executed
+    //The value of the expression is compared with the values of each case and if is matched, the associated block of code is executed
+    //When is reached a break keyword, it breaks out of the switch block and stop the execution of more cases and code.
+    //break and default keywords are optional
+    
+	char x = 'B';
+
+	// switch statements
+	switch (x) {
+	    case 'A':
+		    cout << "Choise is A";
+		    break;
+	    case 'B':
+		    cout << "Choise is B";
+		    break;
+	    case 'C':
+		    cout << "Choise is C";
+		    break;
+    	default:
+		    cout << "Choice other than A, B and C";
+	}
+	
+    int var1 = 1;
+    int var2 = 0;
+  
+    // outer switch
+    switch (var1) {
+        case 0:
+            cout << "Outer Switch Case 0\n";
+            break;
+        case 1:
+            cout << "Outer Switch Case 1\n";
+            // inner switch
+            switch (var2) {
+                case 0:
+                cout << "Inner Switch Case 0\n";
+                break;
+            }
+            break;
+        default:
+            cout << "Default Case of Outer Loop";
+    }
+    
+	return 0;
+}
