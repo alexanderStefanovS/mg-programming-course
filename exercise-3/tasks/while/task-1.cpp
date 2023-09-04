97f1857bf497ffc12e614becdf75c306a20069af
fnadezhda <fnadezhda@vmware.com>
2023-09-04 14:31:30 +0300

    Adding exercise 3

---
+++ exercise-3/tasks/while/task-1.cpp
@@ -0,0 +1,15 @@
+// 2. Извеждане на всички четни числа от 1 до 20:
+ 
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int j = 0;
+    while (j <= 20) {
+        cout << j << " ";
+        j += 2;
+    }
+
+    return 0;
+}
