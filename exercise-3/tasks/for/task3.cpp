9de278b4b508f611e62082066d2d67aec7b4029b
MariyaEM1 <72265069+MariyaEM1@users.noreply.github.com>
2023-09-04 21:59:35 +0300

    Create task3.cpp

---
+++ exercise-3/tasks/for/task3.cpp
@@ -0,0 +1,15 @@
+//Принтирайте умножението на 23 с всяко число от 1 до 10.
+
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    for(int i=1; i<=10; i++){
+        cout<<"24 * "<<i<<"\t= "<<24*i<<"\n";
+    }
+    
+    return 0;
+}
